813bcb6db6e89eb907ba9dff8725832f23acb409
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-17 22:24:23 +0530

    main functions to do a quiz

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswers.java
@@ -0,0 +1,15 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswers {
+    public static String question[] = {
+            "Which company owns android?",
+            "Which one is not a programming language",
+            "Where are you watching this video"
+    };
+    public static String choices[][] = {{"Google", "Apple", "Samsung", "Nokia"},
+            {"Java", "Python", "C", "Notepad"},
+            {"Facebook", "Instagram", "Whatsapp", "Youtube"}
+    };
+
+    public static String correctAnswers[] = {"Google", "Notepad", "Youtube"};
+}
